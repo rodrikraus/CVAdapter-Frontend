4db15a0eb4d8dd7af64612510d3c734defdf8e45
Rodrigo Kraus <rodrikraus10@hotmail.com>
2026-03-23 22:59:12 -0300

    fix: comment out unused LOCAL variable in env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-const LOCAL = 'http://localhost:8080'
+//const LOCAL = 'http://localhost:8080'
 const AZURE = 'https://cvadapter.azurewebsites.net'
 
 export const API = AZURE;
