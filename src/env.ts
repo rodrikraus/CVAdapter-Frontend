111e505c4f09f836d2ca092842fe5587f7fd893b
Rodrigo Kraus <rodrikraus10@hotmail.com>
2026-03-23 22:40:40 -0300

    fix: correct Azure API URL and remove unused Railway variable

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const LOCAL = 'http://localhost:8080'
-const RAILWAY = 'https://cvadapter.up.railway.app'
-const AZURE = 'https://https://cvadapter.azurewebsites.net/'
+const AZURE = 'https://cvadapter.azurewebsites.net'
 
 export const API = AZURE;
